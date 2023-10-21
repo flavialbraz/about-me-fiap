6b74ecd9ebae72e23a49c8e71a95cbfd748ea545
Flavia <flaviabrazx@gmail.com>
2023-10-21 15:10:17 -0300

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-import Header from './components/Header';
-import Tecs from './components/Tecs';
+// import Header from './components/Header';
+// import Tecs from './components/Tecs';
 import About from './components/About';
-import Projects from './components/Projects';
-import MoreAbout from './components/MoreAbout';
-import Footer from './components/Footer';
+// import Projects from './components/Projects';
+// import MoreAbout from './components/MoreAbout';
+// import Footer from './components/Footer';
 
 
 function App() {
@@ -16,7 +16,6 @@ function App() {
         <About />
         {/* <Projects />
         <MoreAbout /> */}
-
         {/* <Footer /> */}
       </div>
   )
